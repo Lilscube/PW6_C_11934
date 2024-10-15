e2021eb5c4da4744ee2b92a85b9e9f33e683d6d2
Lilscube <220711934@students.uajy.ac.id>
2024-10-15 11:19:33 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,9 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('homepage');
+});
+
+Route::resource('/product', App\Http\Controllers\ProductController::class);
